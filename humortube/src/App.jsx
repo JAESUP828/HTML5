34bec220df7ae1d21c1e2e41fe3211de1d10c6c2
JAESUP828 <jaesup828@naver.com>
2025-06-26 11:15:33 +0900

    동영상 올림

---
+++ humortube/src/App.jsx
@@ -0,0 +1,25 @@
+function App() {
+
+  const apple = 1;
+
+
+  return (
+    <div>
+      <h1>웃기는 영상 모음집</h1>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/UuuUJunyqVI?si=hf1eI_NYrqpnaG0D&amp;start=3" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      <br />
+      <br />
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/Zd1SgWqmDlc?si=BDAIY8zTYYvYNgfj&amp;start=100" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/Z7yqs1v3CCI?si=Yn6GBAwONMWgAp6V" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+
+    <h1>사진 모음집</h1>
+
+
+    </div>
+
+  )
+}
+
+export default App
